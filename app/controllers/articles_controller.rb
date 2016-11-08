f9a0e09b6c747817c36707c5c5be05bc02cdb954
Audrey Labuxiere <audreylabuxiere@gmail.com>
2016-11-08 16:58:31 +0100

    added wiki management

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_task)
+    @article.save
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(params_article)
+  end
+
+  def destroy
+    @article.destroy
+  end
+
+  private
+
+  def set_article
+   @article = Article.find(params[:id])
+  end
+
+  def params_article
+    params[:article].permit(:title, :content)
+  end
+end
